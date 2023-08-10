ff8bcef42c75ec277b63d414de7b1dceb2863a8d
Nick Hastings <nshastings@gmail.com>
2023-08-10 18:27:53 -0400

    ❓

--- Resources/NetHook2/SetupDependencies.cmd
+++ Resources/NetHook2/SetupDependencies.cmd
@@ -18,7 +18,7 @@ echo "PATH=%PATH%"
 
 where.exe /q vcpkg
 if %ERRORLEVEL%==1 (
-    if defined GITHUB_ACTIONS (
+    if "%CI%" == "true" (
         if not exist "vcpkg/" (
             git clone --depth 1 "https://github.com/Microsoft/vcpkg.git"
         ) else (
@@ -28,8 +28,8 @@ if %ERRORLEVEL%==1 (
         )
 
         call ".\vcpkg\bootstrap-vcpkg.bat"
-        call :add_curr_dir_to_path
 
+        SET "PATH=%CD%\vcpkg;%PATH%"
         vcpkg integrate install
     ) else (
         echo "vcpkg is required but not found. Please see https://vcpkg.io/en/getting-started to install it"
@@ -37,9 +37,5 @@ if %ERRORLEVEL%==1 (
 )
 
 echo "PATH=%PATH%"
-
-:add_curr_dir_to_path
-SET "PATH=%CD%\vcpkg;%PATH%"
-EXIT /B 0
 
 rem todo: compile or download protoc and generate steammessages_base.pb.{h|cpp}
